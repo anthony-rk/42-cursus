cd7087174be1a56976d375ce1dc643015893d521
anthony-rk <anthonyrkowalski@gmail.com>
2020-11-14 15:06:29 -0800

    Added working ft_split.c file

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -12,82 +12,70 @@
 
 #include "libft.h"
 
-static unsigned int    get_num_words(char const *s, char c)
+static unsigned int			get_num_words(char const *s, char c)
 {
-    unsigned int words;
+	unsigned int words;
 
-    words = 0;
-    while (*s)
-    {
-        if (*s == c)
-        {
-            words++;
-            s++;
-        }
-        s++;
-    }
-    if (*s == '\0')
-      words++;
-    return (words);
+	words = 0;
+	while (*s)
+	{
+		if (*s == c && *(s + 1) != c && *(s + 1) != '\0')
+		{
+			words++;
+			s++;
+		}
+		s++;
+	}
+	if (*s == '\0' && *(s - 1) != c)
+		words++;
+	return (words);
 }
 
-static unsigned int		get_next_word_len(const char *s, char c, unsigned int i)
+static unsigned int			get_next_word_len(const char *s, char c)
 {
-	// loop over s from i until it hits c, then return the diff between i and c, the strlen so to speak
-	unsigned int	start;
+	unsigned int	i;
 
-	start = i;
+	if (*s == c)
+		s++;
+	i = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
-			break;
+			break ;
 		i++;
 	}
-	return (i - start);
+	return (i);
 }
 
-char                    **ft_split(char const *s, char c)
+char						**ft_split(char const *s, char c)
 {
-    char            **split_result;
-    unsigned int    num_strings;
-    unsigned int    i;
-    unsigned int    next_str_len;
-    char            *next_str;
+	char			**split_result;
+	unsigned int	num_strings;
+	unsigned int	i;
+	unsigned int	next_str_len;
+	char			*next_str;
 
-    if (!s)
-        return (NULL);
-    
-    num_strings = get_num_words(s, c);
-
-    split_result = (char **)malloc(sizeof(char *) * (num_strings + 1));
-
-    if (!split_result)
-        return (NULL);
-
-    // Loop over the s string, find the length of the string, then strlcpy() with malloc()
-
-    i = 0;
-    next_str_len = 0;
-    next_str = (char *)s;
-    while (i < num_strings)
-    {
-        // get next word function,
-		
-		// Issue where this len is too long by +1
-        next_str_len = get_next_word_len(next_str, c, next_str_len);
-
-        // Issue with this line below
-        split_result[i] = (char *)malloc(sizeof(char) * (next_str_len + 1));
-
-        if (!split_result[i])
-            return (NULL);
-
-        // copy the string over to split_result[j]
-		if (i != 0)
-			next_str += next_str_len;
-        ft_strlcpy(split_result[i], next_str, next_str_len + 1);
-        i++;
-    }
-    split_result[i] = NULL;
-    return (&*split_result);
+	if (!s)
+		return (NULL);
+	num_strings = get_num_words(s, c);
+	split_result = (char **)malloc(sizeof(char *) * (num_strings + 1));
+	if (!split_result)
+		return (NULL);
+	i = 0;
+	next_str_len = 0;
+	next_str = (char *)s;
+	while (i < num_strings)
+	{
+		while (*next_str == c)
+			next_str++;
+		next_str_len = get_next_word_len(next_str, c);
+		split_result[i] = (char *)malloc(sizeof(char) * (next_str_len + 1));
+		if (!split_result[i])
+			return (NULL);
+		ft_strlcpy(split_result[i], next_str, next_str_len + 1);
+		i++;
+		next_str += next_str_len + 1;
+	}
+	split_result[i] = NULL;
+	return (&*split_result);
 }
