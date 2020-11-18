d88c5c0038c851faf835378cbe89b3fe9ec61d0c
anthony-rk <anthonyrkowalski@gmail.com>
2020-11-17 22:22:42 -0800

    Update to handle a Null pointer as the list pointer

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -16,10 +16,12 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last_node;
 
-	if (lst && (*lst) && new)
+	if (!*lst)
 	{
-		last_node = ft_lstlast(*lst);
-		last_node->next = &(*new);
-		new->next = NULL;
+		*lst = new;
+		return ;
 	}
+	last_node = ft_lstlast(*lst);
+	last_node->next = new;
+	new->next = NULL;
 }
