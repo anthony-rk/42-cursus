71938970b4ad967af0b4721dd375286ef260a418
anthony-rk <anthonyrkowalski@gmail.com>
2020-11-22 12:44:31 -0800

    Add handler for NULL params

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -16,6 +16,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	unsigned int	i;
 
+	if (!dst && !src)
+		return (0);
 	if (dstsize == 0)
 		return (ft_strlen(src));
 	i = 0;
@@ -24,8 +26,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		dst[i] = src[i];
 		i++;
 	}
-	if (i < dstsize)
-		dst[i] = '\0';
+	dst[i] = '\0';
 	while (src[i] != '\0')
 		i++;
 	return (i);
