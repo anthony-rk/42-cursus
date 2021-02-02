9489b6de5dc6b2f8a46bd51f62d2abb726594d55
anthony-rk <anthonyrkowalski@gmail.com>
2021-02-01 17:28:09 -0800

    Add test main.c file

---
+++ get_next_line/main.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akowalsk <akowalsk@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/01 17:27:12 by akowalsk          #+#    #+#             */
+/*   Updated: 2021/02/01 17:27:29 by akowalsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include "get_next_line.h"
+
+int main(int argc, char **argv)
+{
+	int fd;
+	int ret;
+	int line;
+	char *buff;
+
+	line = 0;
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		while ((ret = get_next_line(fd, &buff)) > 0)
+		{
+			printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+			free(buff);
+		}
+		printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+		if (ret == -1)
+			printf("-----------\nError\n");
+		else if (ret == 0)
+			printf("-----------\nEnd of file\n");
+		close(fd);
+	}
+	if (argc == 1)
+	{
+		while ((ret = get_next_line(0, &buff)) > 0)
+			printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+		if (ret == -1)
+			printf("-----------\nError\n");
+		else if (ret == 0)
+			printf("-----------\nEnd of stdin\n");
+		close(fd);
+	}
+	return (0);
+}
