7df9608fc38d0add42429444f4d9ae9e96657cfd
anthony-rk <anthonyrkowalski@gmail.com>
2021-02-12 19:08:36 -0800

    Update to stop memory leaks

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <fcntl.h>
+#include <fcntl.h> // For 0_RDONLY in open()
 #include <sys/uio.h>
 #include <sys/types.h>
 #include "get_next_line.h"
@@ -24,6 +24,7 @@ int		main(int argc, char **argv)
 	char	*buff;
 
 	line = 0;
+	buff = NULL;
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
@@ -33,6 +34,7 @@ int		main(int argc, char **argv)
 			free(buff);
 		}
 		printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+		free(buff);
 		if (ret == -1)
 			printf("-----------\nError\n");
 		else if (ret == 0)
@@ -42,7 +44,10 @@ int		main(int argc, char **argv)
 	if (argc == 1)
 	{
 		while ((ret = get_next_line(0, &buff)) > 0)
+		{
 			printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
+			free(buff);
+		}
 		if (ret == -1)
 			printf("-----------\nError\n");
 		else if (ret == 0)
