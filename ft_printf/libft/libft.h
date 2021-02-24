d1e90abe1c2b88907e8be830beb99143baa11742
anthony-rk <anthonyrkowalski@gmail.com>
2021-02-23 17:25:12 -0800

    Add prototypes for additional functions

--- ft_printf/libft/libft.h
+++ ft_printf/libft/libft.h
@@ -67,4 +67,10 @@ void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst,
 		void *(*f)(void *), void (*del)(void *));
+
+// Newly added functions
+char				*ft_itoa_base(int n, int base, int uppercase);
+char				*ft_strrev(char *str);
+int					ft_nbrlen(int n, int base);
+
 #endif
