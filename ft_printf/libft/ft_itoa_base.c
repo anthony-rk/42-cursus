9c0742f4c1545ea8a4648bfc4970a4b79607fe82
anthony-rk <anthonyrkowalski@gmail.com>
2021-02-23 17:28:38 -0800

    removed commented code

--- ft_printf/libft/ft_itoa_base.c
+++ ft_printf/libft/ft_itoa_base.c
@@ -6,24 +6,11 @@
 /*   By: akowalsk <akowalsk@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 16:54:48 by akowalsk          #+#    #+#             */
-/*   Updated: 2021/02/23 17:09:01 by akowalsk         ###   ########.fr       */
+/*   Updated: 2021/02/23 17:28:15 by akowalsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// static unsigned int			ft_num_len(int n, int base)
-// {
-// 	unsigned int	len;
-
-// 	len = 0;
-// 	while (n)
-// 	{
-// 		n /= base;
-// 		len++;
-// 	}
-// 	return (len);
-// }
 
 char	*ft_itoa_base(int n, int base, int uppercase)
 {
